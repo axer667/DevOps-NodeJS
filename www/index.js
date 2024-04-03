c86a76e5b67524a844f1382d9b67071edcc754a1
Misha <vellsevul@mail.ru>
2024-04-03 10:42:49 +0300

    Add fs and use html-file

--- www/index.js
+++ www/index.js
@@ -1,8 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 http.createServer(function (request, response) {
-    response.write("String 1\n");
-    response.write("String 2\n");
-    response.write("String 3\n");
-    response.end("String end");
+    if (request.url === "/user") {
+        let data = "";
+        request.on("data", chunk => {
+            data += chunk;
+        });
+        request.on("end", () => {
+            console.log(data);
+            response.end("done: " + data);
+        });
+    } else {
+        fs.readFile("index.html", "utf8", function (error, data) {
+            const message = "Какой-то текст";
+            const header = "Заголовок";
+            data = data.replace("{header}", header).replace("{message}", message);
+            response.end(data);
+        })
+    }
 }).listen(3000);
